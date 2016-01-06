2c578aa39a8c96388583c9a1a68f309cc893d714
unknown <shihaijiang@ZT-2200356.ztgame.com>
2016-01-06 17:12:14 +0800

    add project for XiaoWangDaLai

---
+++ xiaowangdalai/xiaowangdalai/index.js
@@ -0,0 +1,39 @@
+﻿var $slide = $('.slide'),
+    $slideGroup = $('.slideup');
+
+var slidesTotal = ($slide.length - 1),
+    current = 0;
+
+var updateIndex = function () 
+{
+    if (current === slidesTotal)
+    {
+        current = 0;
+    }
+    else
+    {
+        current++;
+    }
+    transition(current);
+};
+
+var transition = function (slidePosition) {
+    $slideGroup.animate({
+        'top': '-' + slidePosition + '7%'
+    });
+};
+
+window.setInterval(updateIndex, 2000);
+
+function btnTopHover() {
+    window.location.href = "http://mp.weixin.qq.com/s?__biz=MzI2NjAwOTEyMw==&mid=414512791&idx=1&sn=96bb6ec7001d58a65ead8fb805248041#rd";
+}
+function btnRegister() {
+    window.location.href = "http://www.uncn88.com/reg";
+}
+function btnNotifyNext() {
+    $('.shareBackground').show();
+}
+function btnCloseShare() {
+    $('.shareBackground').hide();
+}
---
+++ xiaowangdalai/xiaowangdalai/wx_share.js
@@ -0,0 +1,54 @@
+﻿var imgUrl = "http://ww4.sinaimg.cn/mw690/005RI5OBgw1ezpkqbomy0j308c08cdge.jpg";  //图片LOGO注意必须是绝对路径
+var lineLink = "http://ww4.sinaimg.cn/mw690/";   //网站网址，必须是绝对路径
+var descContent = '小往大来新手活动棒棒哒，理财平台稳健省心最重要。'; //分享给朋友或朋友圈时的文字简介
+var shareTitle = '良心老板喊你来领年终奖，全额8888元';  //分享title
+var appid = ''; //apiID，可留空
+
+function shareFriend() {
+    WeixinJSBridge.invoke('sendAppMessage', {
+        "appid": appid,
+        "img_url": imgUrl,
+        "img_width": "200",
+        "img_height": "200",
+        "link": lineLink,
+        "desc": descContent,
+        "title": shareTitle
+    }, function (res) {
+        //_report('send_msg', res.err_msg);
+    })
+}
+function shareTimeline() {
+    WeixinJSBridge.invoke('shareTimeline', {
+        "img_url": imgUrl,
+        "img_width": "200",
+        "img_height": "200",
+        "link": lineLink,
+        "desc": descContent,
+        "title": shareTitle
+    }, function (res) {
+        //_report('timeline', res.err_msg);
+    });
+}
+function shareWeibo() {
+    WeixinJSBridge.invoke('shareWeibo', {
+        "content": descContent,
+        "url": lineLink,
+    }, function (res) {
+        //_report('weibo', res.err_msg);
+    });
+}
+// 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
+document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+    // 发送给好友
+    WeixinJSBridge.on('menu:share:appmessage', function (argv) {
+        shareFriend();
+    });
+    // 分享到朋友圈
+    WeixinJSBridge.on('menu:share:timeline', function (argv) {
+        shareTimeline();
+    });
+    // 分享到微博
+    WeixinJSBridge.on('menu:share:weibo', function (argv) {
+        shareWeibo();
+    });
+}, false);
