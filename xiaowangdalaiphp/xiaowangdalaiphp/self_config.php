dd3f4dd1d7321bee4544d9f615ae853dd2cc00d7
unknown <shihaijiang@ZT-2200356.ztgame.com>
2016-01-14 18:51:12 +0800

    增加一个记录文件

---
+++ xiaowangdalaiphp/xiaowangdalaiphp/self_config.php
@@ -0,0 +1,6 @@
+<?php 
+    //不是网页用的，记录一些临时的事情
+	//$jssdk = new JSSDK("wx580cbdc1e130c188", "43d6e288cef7049055b51b69a668aa8d"); //服务号
+    //$jssdk = new JSSDK("wx2ad2432b80e8b1ce", "692d967e1436ddb28b0c6d4999de5509");   //个人未认证号
+    //$jssdk = new JSSDK("wxcf10daf7961c51d2", "59e03b3d8d1ebd261d8ab5a1bed47dbe ");   //已认证订阅号
+?>
